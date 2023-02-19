ffa5f810184b227403fa776d9238858ef569bd15
crg85-ua <crg85@alu.ua.es>
2023-02-19 14:04:46 +0100

    Migraciones modificadas

--- database/migrations/2023_02_19_122058_create_restaurante_table.php.php
+++ database/migrations/2023_02_19_122058_create_restaurante_table.php.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('restaurante', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('telefono');
+            $table->string('descripcion');
         });
     }
 
